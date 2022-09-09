f15210e0631906bb06e92c7693f1e9c1f80df69c
NiCHUY <89972933+NiCHUY@users.noreply.github.com>
2022-09-09 21:58:49 +0300

    Update Series.java

--- Lab_1/src/Series.java
+++ Lab_1/src/Series.java
@@ -1,27 +1,47 @@
 import java.math.BigDecimal;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 //Variant №1
 public class Series {
 public static void main(String[] args) {
+    BigDecimal[] arguments;
 //Input your values
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Enter your x");
-    BigDecimal x = scanner.nextBigDecimal();
-    System.out.println("Enter your precision");
-    BigDecimal precision = scanner.nextBigDecimal();
-    scanner.close();
+    arguments = GetArguments();
 //Initializing of new values
     BigDecimal result = new BigDecimal(0);
     BigDecimal term;
     int k = 1;
-    term = x.pow((3 * (int)Math.pow(k , 2)));
+    term = arguments[0].pow((3 * (int)Math.pow(k , 2)));
 //Series loop
-    while(term.compareTo(precision) > 0)
+    while(term.compareTo(arguments[1]) > 0)
     {
-        result= result.add(term);
+        result = result.add(term);
         k++;
-        term = x.pow((3 * (int)Math.pow(k , 2)));
+        term = arguments[0].pow((3 * (int)Math.pow(k , 2)));
     }
     System.out.println(result);
+    }
+
+    static BigDecimal[] GetArguments() {
+        BigDecimal[] arguments;
+        try {
+            arguments = new BigDecimal[2];
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter your x and precision:");
+            String str = scanner.nextLine();
+            String[] args = str.split(" ");
+            if (args.length != 2) {
+                throw new ArrayIndexOutOfBoundsException("Too many or not enough arguments");
+            }
+            arguments[0] = new BigDecimal(args[0]);
+            arguments[1] = new BigDecimal(args[1]);
+            scanner.close();
+            if (arguments[0].abs().compareTo(new BigDecimal("1")) >= 0) {
+                throw new IllegalArgumentException("Please input x in (-1, 1)");
+            }
+        } catch (NumberFormatException ex) {
+            throw new InputMismatchException("Please input correct");
+        }
+        return arguments;
     }
 }
