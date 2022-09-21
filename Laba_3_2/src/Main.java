13e34d3a2ea0af38b2cecad91be1258c85fdd7c1
NiCHUY <89972933+NiCHUY@users.noreply.github.com>
2022-09-21 15:02:21 +0300

    Add files via upload

---
+++ Laba_3_2/src/Main.java
@@ -0,0 +1,73 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) throws InputException {
+
+        ArrayList<Integer> indexes1 = new ArrayList<>();
+        ArrayList<Integer> indexes2 = new ArrayList<>();
+        StringBuffer string = new StringBuffer(args[0]);
+
+        for (int i = 0; i < string.length(); i++){
+            if (string.charAt(i) == '('){
+                indexes1.add(i);
+            }
+            if (string.charAt(i) == ')'){
+                indexes2.add(i);
+            }
+        }
+
+        if (indexes1.get(0) > indexes2.get(0) || indexes1.size() != indexes2.size()){
+            throw new InputException("Input is not correct");
+        }
+
+        for (int index = 0; index < indexes1.size(); index++){
+            int count = 0;
+            int indexNew = 0;
+            for(int i = 0; i < indexes1.size(); i++){
+                if (indexes1.get(i) < indexes2.get(0)) {
+                    count++;
+                    indexNew++;
+                }
+            }
+
+            if (count == 1){
+                string.delete(indexes1.get(indexNew - 1), indexes2.get(indexNew - 1) + 1);
+                int diff = indexes2.get(indexNew - 1) - indexes1.get(indexNew - 1) +1;
+                for(int i = 0; i < indexes1.size(); i++){
+                    indexes1.set(i, indexes1.get(i) - diff);
+                    indexes2.set(i, indexes2.get(i) - diff);
+                }
+                indexes1.remove(0);
+                indexes2.remove(0);
+                index = 0;
+            }
+
+            if (count / 2 > 0){
+                string.delete(indexes1.get(0), indexes1.get(1) + 1);
+                int diff = indexes1.get(1) - indexes1.get(0)+1;
+                for(int i = 0; i < indexes1.size(); i++){
+                    indexes1.set(i, indexes1.get(i) - diff);
+                    indexes2.set(i, indexes2.get(i) - diff);
+                }
+                string.delete(indexes2.get(count-2), indexes2.get(count-1) + 1);
+                diff = indexes2.get(count-1) - indexes2.get(count-2) + 1;
+                for(int i = count; i < indexes1.size(); i++){
+                    indexes1.set(i, indexes1.get(i) - diff);
+                    indexes2.set(i, indexes2.get(i) - diff);
+                }
+                indexes1.remove(0);
+                indexes2.remove(count - 2);
+                indexes1.remove(0);
+                indexes2.remove(count - 2);
+                index = 0;
+            }
+        }
+        if (indexes1.size() == 1){
+            string.delete(indexes1.get(0), indexes2.get(0) + 1);
+            indexes1.remove(0);
+            indexes2.remove(0);
+        }
+        System.out.println(string);
+        System.out.println(indexes1.size());
+    }
+}
